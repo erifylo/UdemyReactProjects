0edb7f03a5d290836590ed677f07669a921bb65c
erifylo <erif.evangelou@gmail.com>
2021-01-06 17:11:56 +0100

    [UPDATED] progect 9

--- 9-color-generator/setup/src/SingleColor.js
+++ 9-color-generator/setup/src/SingleColor.js
@@ -1,17 +1,32 @@
-import React, { useState, useEffect } from 'react'
-import rgbToHex from './utils'
+import React, { useState, useEffect } from "react";
+import rgbToHex from "./utils";
 
-const SingleColor = ({rgb, weight, index}) => {
-  const [alert, setAlert] = useState(false)
-  const bcg = rgb.join(",")
-  const hex = rgbToHex(...rgb)
+const SingleColor = ({ rgb, weight, index }) => {
+  const [alert, setAlert] = useState(false);
+  const bcg = rgb.join(",");
+  const hex = rgbToHex(...rgb);
 
-  return <article className={`color`} style={{backgroundColor:`rgb(${bcg})`}}>
-    <p className="percent-value">
-      {weight}%
-    </p>
-    <p className="color-value"></p>
-  </article>
-}
+useEffect(() => {
+const timeout = setTimeout(() => {
+  setAlert(false)
+}, 2000)
+return () => clearTimeout(timeout)
+}, [alert])
 
-export default SingleColor
+  return (
+    <article
+      className={`color ${index > 10 && "color-light"}`}
+      style={{ backgroundColor: `rgb(${bcg})` }} 
+      onClick={() => {
+        setAlert(true);
+        navigator.clipboard.writeText(hex)
+      }}
+    >
+      <p className="percent-value">{weight}%</p>
+      <p className="color-value">{hex}</p>
+      {alert && <p className="alert">copied to clipboard</p>}
+    </article>
+  );
+};
+
+export default SingleColor;
