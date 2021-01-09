adeb5840f9509cae32d02249e81bc282dec4dc29
erifylo <erif.evangelou@gmail.com>
2021-01-09 20:13:55 +0100

    started the navbar project, sidebar to be fixed

---
+++ 11-navbar/setup/src/Sidebar.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function Sidebar() {
+    return (
+        <div className="links-container show-container">
+        <ul className="links">
+          <li>
+            <a href="#">home</a>
+            <a href="#">about</a>
+            <a href="#">contact</a>
+            <a href="#">products</a>
+          </li>
+        </ul>
+        </div>
+    )
+}
