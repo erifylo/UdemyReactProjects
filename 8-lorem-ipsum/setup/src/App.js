a5e0cbca2364cd816a98845d55170c04d6a09bbf
erifylo <erif.evangelou@gmail.com>
2021-01-23 11:48:21 +0100

    addition in lorem-ipsum project

--- 8-lorem-ipsum/setup/src/App.js
+++ 8-lorem-ipsum/setup/src/App.js
@@ -7,34 +7,35 @@ function App() {
   const handleSubmit = (e) => {
     e.preventDefault();
     let amount = parseInt(count);
-    if (amount <= 0){
-      amount = 1
- 
-    } 
-    if (amount > 8){
-      amount = 6;
-  
-    } 
-    setText(data.slice(0, amount))
-  
-  }
+    if (count <= 0) {
+      amount = 1;
+    }
+    if (count > 8) {
+      amount = 8;
+    }
+    setText(data.slice(0, amount));
+  };
   return (
- <section className="section-center">
-   <h3>tired of boring lorem ipsum?</h3>
-   <form className="lorem-form" onSubmit={handleSubmit}>
-   <label htmlFor="amount">
-     paragraphs
-   </label>
-   <input type="number" name="amount" id="amount" value={count} onChange={(e) => setCount(e.target.value)}/>
-   <button type="submit" className="btn">generate</button>
-   </form>
-   <article className="lorem-text">
-     {text.map((item,index) => {
-       return <p key={index}>{item}</p>
-     })}
-     </article>
- </section> 
-    )
+    <section className='section-center'>
+      <h3>tired of boring lorem ipsum?</h3>
+      <form className='lorem-form' onSubmit={handleSubmit}>
+        <label htmlFor='amount'>paragraphs:</label>
+        <input
+          type='number'
+          name='amount'
+          id='amount'
+          value={count}
+          onChange={(e) => setCount(e.target.value)}
+        />
+        <button className='btn'>generate</button>
+      </form>
+      <article className='lorem-text'>
+        {text.map((item, index) => {
+          return <p key={index}>{item}</p>;
+        })}
+      </article>
+    </section>
+  );
 }
 
 export default App;
